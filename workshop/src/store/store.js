694cdcfe5ed4575ae5cd0a9b525620a52759ce9c
Jakub <jakub.wilkowski@10clouds.com>
2019-03-16 14:58:02 +0100

    [#4.3] Change User module to persistent module(stored in localStorage - vuex-persist package).

--- workshop/src/store/store.js
+++ workshop/src/store/store.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
-import users from './modules/user.module';
+import users from './modules/user.module'
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   modules: {
     users
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
