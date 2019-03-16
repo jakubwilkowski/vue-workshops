a1b51b1ff49671c7e5255019001665fe813b112a
Jakub <jakub.wilkowski@10clouds.com>
2019-03-16 12:14:50 +0100

    [#2.2] Add BaseInput component with custom v-model (should also contain label).

---
+++ workshop/src/components/BaseComponents/BaseInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <label for="">
+    <input
+      type="text"
+      :value="value"
+      :style="`background-color: ${backgroundColor}`"
+      @input="inputAction"
+    >
+  </label>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: 'BaseInput',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    backgroundColor: {
+      required: false
+    }
+  },
+  methods: {
+    inputAction (event) {
+      console.log('emit')
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- workshop/src/views/Vote.vue
+++ workshop/src/views/Vote.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="vote">
-
+    <BaseInput
+      v-model="inputValue"
+      backgroundColor="#00F"
+    >
+    </BaseInput>
+    <h4>{{ inputValue }}</h4>
   </div>
 </template>
 
 <script>
+import BaseInput from '@/components/BaseComponents/BaseInput.vue'
+
 export default {
   name: 'Vote',
-
+  data () {
+    return {
+      inputValue: 'dupa'
+    }
+  },
+  components: {
+    BaseInput
+  }
 }
 </script>
